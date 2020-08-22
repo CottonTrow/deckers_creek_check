c4586ab445d5588f1a19b48f678b9628ac208306
CottonTrow <Bilyhasemail@gmail.com>
2020-08-22 07:24:14 -0400

    Figured out how to find data

--- usgscrape.py
+++ usgscrape.py
@@ -12,9 +12,4 @@ soup = bs.BeautifulSoup (source, 'lxml')
 for wml2 in soup.find_all('wml2:value'):
     print(wml2.string)
 
-#remnants of testing
-'''for paragraph in soup.find_all('p'):
-    print(paragraph.string)'''
-
-#print(soup.get_text())
 
