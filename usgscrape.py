b463ddb014d5e611c7c3a012c664e7b2c2ac5237
CottonTrow <Bilyhasemail@gmail.com>
2020-08-22 13:09:55 -0400

    Figured out how to find, display, and analyze data from a website

--- usgscrape.py
+++ usgscrape.py
@@ -3,10 +3,11 @@ This is a fun project to teach Bily how to code, with a shit ton of help from Ar
 #importing modules
 import bs4 as bs
 import urllib.request
+import time
 
 #location of desired information in the series of tubes
 source = urllib.request.urlopen('https://waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&sites=03062500&parameterCd=00060&siteType=ST&siteStatus=all') .read()
-soup = bs.BeautifulSoup (source, 'lxml')
+soup = bs.BeautifulSoup(source, 'lxml')
 
 #the location in the website of the data we desire
 for wml2 in soup.find_all('wml2:value'):
@@ -16,7 +17,9 @@ for wml2 in soup.find_all('wml2:value'):
 #creating my analysis
 cfs = float(wml2.string)
 
-if cfs > 270 and 600 > cfs:
+if 270 < cfs < 600:
     print("Decker's is running. You should go paddling")
 else: print("Maybe you should find something else to do.")
+
+time.sleep(15)
 
