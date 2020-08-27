5bb59cac380e32fe24ca4b97fa159572ca5a2fb1
CottonTrow <Bilyhasemail@gmail.com>
2020-08-26 20:39:58 -0400

    playing with data from multiple websites, and looking at multiple days of data

---
+++ lookingatayearofdata.py
@@ -0,0 +1,33 @@
+'''A program to check the last updated CFS level for Decker's Creek
+This is a fun project to teach Bily how to code, with a shit ton of help from Arthur Elmes'''
+#importing modules
+import bs4 as bs
+import urllib.request
+from collections import Counter
+
+#location of desired information in the series of tubes
+source = urllib.request.urlopen('https://nwis.waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&sites=03062500&period='
+                                'P365D&parameterCd=00060&siteType=ST&siteStatus=all') .read()
+soup = bs.BeautifulSoup(source, 'xml')
+#the list that will hold all of the values
+cfs = []
+runnablecfs = []
+#the location in the website of the data we desire
+for wml2 in soup.find_all('wml2:value'):
+#propogating the list
+    cfs.append(float(wml2.string))
+#print(len(cfs))
+print(type(cfs))
+print(len(cfs))
+print(len(runnablecfs))
+
+for x in cfs:
+    if 270 < x < 600:
+        runnablecfs.append(x)
+
+print(type(cfs))
+print(len(cfs))
+print(len(runnablecfs))
+
+
+
--- multiple website scrape.py
+++ multiple website scrape.py
@@ -4,10 +4,14 @@ import urllib.request
 import time
 
 #location of desired information in the series of tubes
-source = urllib.request.urlopen('https://waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&sites=03062500&parameterCd=00060&siteType=ST&siteStatus=all').read()
+'''source = urllib.request.urlopen('https://waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&sites=03062500&
+parameterCd=00060&siteType=ST&siteStatus=all').read()
+source1 = urllib.request.urlopen('https://waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&sites=03066000&parameterCd=
+00060&siteType=ST&siteStatus=all').read()
+source2 = urllib.request.urlopen('https://waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&sites=03070260&parameterCd=
+00060&siteType=ST&siteStatus=all').read()
+
 soup = bs.BeautifulSoup(source, 'xml')
-source1 = urllib.request.urlopen('https://waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&sites=03066000&parameterCd=00060&siteType=ST&siteStatus=all').read()
-source2 = urllib.request.urlopen('https://waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&sites=03070260&parameterCd=00060&siteType=ST&siteStatus=all').read()
 soup1 = bs.BeautifulSoup(source1, 'xml')
 soup2 = bs.BeautifulSoup(source2, 'xml')
 
@@ -22,4 +26,15 @@ for wml2 in soup1.find_all('wml2:value'):
 
 for wml2 in soup2.find_all('wml2:value'):
     print('The level of the Cheat is currently:')
-    print(wml2.string)
+    print(wml2.string)'''
+
+url_dict = {'link 1': 'https://waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&sites=03062500&parameterCd=00060&siteT'
+                      'ype=ST&siteStatus=all', 'link 2': 'https://waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&sit'
+                      'es=03066000&parameterCd=00060&siteType=ST&siteStatus=all', 'link 3': 'https://waterservices.usgs.'
+                      'gov/nwis/iv/?format=waterml,2.0&sites=03070260&parameterCd=00060&siteType=ST&siteStatus=all'}
+for x in url_dict:
+    print(url_dict[x])
+for x in url_dict:
+    print(x)
+for k, v in url_dict.items():
+    print(k, v)
