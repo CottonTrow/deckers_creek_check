f7d03e4841a5ac2f5cfe802c40c7a4eaaea24de6
CottonTrow <Bilyhasemail@gmail.com>
2020-09-16 07:03:27 -0400

    playing with data from multiple websites, and looking at multiple days of data

--- lookingatayearofdata.py
+++ lookingatayearofdata.py
@@ -3,14 +3,16 @@ This is a fun project to teach Bily how to code, with a shit ton of help from Ar
 #importing modules
 import bs4 as bs
 import urllib.request
-from collections import Counter
+import datetime
+import numpy as np
+import pandas as pd
 
 #location of desired information in the series of tubes
 source = urllib.request.urlopen('https://nwis.waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&sites=03062500&period='
                                 'P365D&parameterCd=00060&siteType=ST&siteStatus=all') .read()
 soup = bs.BeautifulSoup(source, 'xml')
 
-#the list that will hold all of the values
+#the lists that will hold all of the values
 cfs = []
 runnablecfs = []
 timestamp = []
@@ -34,8 +36,10 @@ print(len(runnablecfs))
 for wml2 in soup.find_all('wml2:time'):
     timestamp.append(wml2.string)
 
+#splicing the timestamps and cfs values
 decker_dict = dict(zip(timestamp, cfs))
-print(decker_dict)
+#print(decker_dict)
 
-
-
+#filtering out duplicate day
+"""one possibility is to create a new list for every day and put all entries of that day into its list. Then count the
+number of lists. This would give me a single entry per day."""
